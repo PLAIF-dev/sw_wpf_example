7a7c5bf37ca981671d80fa3f2d85e61fa53be4bb
jongwooparkk <jwpark@plaif.com>
2023-06-30 16:17:03 +0900

    WPF Example Code

---
+++ WpfAppExample/Model/MainModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfAppExample.Model
+{
+    public class MainModel
+    {
+        public class ItemModel
+        {
+            public string ImageSource { get; set; }
+            public string Name { get; set; }
+        }
+    }
+}
---
+++ WpfAppExample/ViewModel/MainVM.cs
@@ -0,0 +1,33 @@
+﻿using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using static WpfAppExample.Model.MainModel;
+
+namespace WpfAppExample.ViewModel
+{
+    public class MainVM : ViewModelBase
+    {
+        public ObservableCollection<ItemModel> ItemModels { get; set; } = new();
+
+        public ICommand AddClick { get; set; }
+
+        public MainVM() 
+        {
+            AddClick = new RelayCommand(AddCommand);
+        }
+
+        public void AddCommand()
+        {
+            ItemModel im = new ItemModel();
+            im.Name = "PLAIF";
+            im.ImageSource = "../Image/plaif.png";
+
+            ItemModels.Add(im);
+        }
+    }
+}
